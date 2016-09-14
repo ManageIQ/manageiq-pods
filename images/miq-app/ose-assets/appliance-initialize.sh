0d5690795fcdb7d2b449e78da97da890308ea328
Franco Bladilo <fbladilo@redhat.com>
2016-09-14 00:35:32 -0400

    Various cleanups and enhancements to initial commit
    - Copy ose-fix-permissions to appropiate SCRIPT_ROOT dir on miq-app Dockerfile
    - Add prepare_svc_vars function to deal with preparation of all SERVICE_HOST vars
    - Added DB migrations for Upgrade/Redeployment tasks in appliance-initialize.sh
    - Moved configuration of memcached into its own function
    - Added setup_persistent_data function as a new_deployment task to appliance-initialize.sh
    - Added proper description to all functions in ose-deploy-common
    - Renamed variables on check_deployment_status for better readability
    - Corrected and added more comments into various functions

--- images/miq-app/ose-assets/appliance-initialize.sh
+++ images/miq-app/ose-assets/appliance-initialize.sh
@@ -5,30 +5,36 @@
 # Ensure OpenShift scripting environment is present, exit if source fails
 [[ -s ${SCRIPTS_ROOT}/ose-deploy-common.sh ]] && source ${SCRIPTS_ROOT}/ose-deploy-common.sh || { echo "Failed to source ${SCRIPTS_ROOT}/ose-deploy-common.sh" ; exit 1; }
 
-# Check deployment status (new, upgrade or redeploy)
+# Prepare service host vars
+prepare_svc_vars
 
+# Check deployment status
 check_deployment_status
 
 if [ -n "${UPGRADE}" ]; then
-  echo "Deployment upgrade (do something) : ${UPGRADE}"
+  echo "== Starting Upgrade =="
+  echo "== Migrating Database =="
+  cd ${APP_ROOT} && bin/rake db:migrate
 fi
 
 if [ -n "${REDEPLOY}" ]; then
-  echo "This is a redeployment (do something) : ${REDEPLOY}"
+  echo "== Starting Re-deployment =="
+  echo "== Migrating Database =="
+  cd ${APP_ROOT} && bin/rake db:migrate
 fi
 
 if [ -n "${NEW_DEPLOYMENT}" ]; then
 
-  # Environment supplied by OpenShift MIQ via template
-  # Assemble DB service host variable based on template parameter $DATABASE_SERVICE_NAME
+  # Setup memcached host in EVM configuration
+  setup_memcached
 
-  DATABASE_SVC_HOST="$(echo $DATABASE_SERVICE_NAME | tr '[:lower:]' '[:upper:]')_SERVICE_HOST"
-  MEMCACHED_SVC_HOST="$(echo $MEMCACHED_SERVICE_NAME | tr '[:lower:]' '[:upper:]')_SERVICE_HOST"
-
-  # Replace memcache host in EVM configuration
-  sed -i.bak -E "s/:memcache_server:.*/:memcache_server: ${!MEMCACHED_SVC_HOST}:11211/gi" ${APP_ROOT}/config/settings.yml
-
-  echo "Initializing Appliance, please wait ..."
+  # Setup persistent data files/dirs on PV
+  setup_persistent_data
+ 
+  # Initialize EVM appliance 
+  echo "== Initializing Appliance =="
   appliance_console_cli --region ${DATABASE_REGION} --hostname ${!DATABASE_SVC_HOST} --username ${POSTGRESQL_USER} --password ${POSTGRESQL_PASSWORD}
+
+  # Write deployment info file to PV
   write_deployment_info
 fi
--- images/miq-app/ose-assets/ose-deploy-common.sh
+++ images/miq-app/ose-assets/ose-deploy-common.sh
@@ -2,46 +2,54 @@
 
 [[ -s /etc/default/evm ]] && source /etc/default/evm
 
-# Define persistent dirs paths
-
+# This file is created by the write_deployment_info during initial deployment
 DEPLOY_INFO_FILE=${APP_ROOT_PERSISTENT}/.deployment_info
 
 function check_deployment_status {
+# Description
+# Inspect PV for previous deployments, if a DB a config is present, restore
+# Source previous deployment info file from PV and compare data with current environment
+# Evaluate conditions and decide a target deployment type: redeploy,upgrade or new
+
+echo "== Checking deployment status =="
 
 if [[ -f ${APP_ROOT_PERSISTENT}/config/database.yml && -f ${DEPLOY_INFO_FILE} ]]; then
   echo "== Found existing deployment configuration =="
   echo "== Restoring existing database configuration =="
-  mv ${APP_ROOT}/config/database.yml ${APP_ROOT}/config/database.yml.bak
+  mv ${APP_ROOT}/config/database.yml ${APP_ROOT}/config/database.yml~
   ln -s ${APP_ROOT_PERSISTENT}/config/database.yml ${APP_ROOT}/config/database.yml
-  # Source original deployment env from persistent file
+  # Source original deployment info variables from PV
   source ${DEPLOY_INFO_FILE}
-  # Obtain currently running env
-  CURRENT_APP_VERSION=$(cat ${APP_ROOT}/VERSION)
-  CURRENT_SCHEMA_VERSION=$(cd ${APP_ROOT} ; bin/rake db:version | awk '{ print $3 }')
-  CURRENT_IMG_VERSION=${IMAGE_VERSION}
-  # Check if we have a redeploy with identical versions (exclude master)
-  if [[ $APP_VERSION == $CURRENT_APP_VERSION && $APP_VERSION != master ]]; then
+  # Obtain current running environment
+  APP_VERSION=$(cat ${APP_ROOT}/VERSION)
+  SCHEMA_VERSION=$(cd ${APP_ROOT} ; bin/rake db:version | awk '{ print $3 }')
+  IMG_VERSION=${IMAGE_VERSION}
+  # Check if we have identical EVM versions (exclude master builds)
+  if [[ $APP_VERSION == $PV_APP_VERSION && $APP_VERSION != master ]]; then
     echo "== App version matches original deployment =="
-    if [ ${SCHEMA_VERSION} != ${CURRENT_SCHEMA_VERSION} ]; then
-       echo "Something seems wrong, db schema version mismatch for the same app version: ${SCHEMA_VERSION} <-> ${CURRENT_SCHEMA_VERSION}"
+  # Check if we have same schema version for same EVM version
+    if [ ${SCHEMA_VERSION} != ${PV_SCHEMA_VERSION} ]; then
+       echo "Something seems wrong, db schema version mismatch for the same app version: ${PV_SCHEMA_VERSION} <-> ${SCHEMA_VERSION}"
        exit 1
     fi
+  # Assuming redeployment (same APP_VERSION)
   export REDEPLOY=true
   else
-  # Assuming different app versions (upgrade)
-  echo "== Migrating Database =="
-  cd ${APP_ROOT} ; bin/rake db:migrate
+  # Assuming upgrade (different APP_VERSION)
   export UPGRADE=true
   fi
 else
-  echo "Could not find pre-existing configuration, assuming new deployment.."
+  echo "No pre-existing EVM configuration found on PV"
   export NEW_DEPLOYMENT=true
-fi	
+fi
+
 }
 
 function write_deployment_info {
-# Populate info file based on initial deployment
-# IMAGE_VERSION is supplied by docker ENV
+# Description
+# Populate info file based on initial deployment and store on PV
+# Output in bash format to be easily sourced
+# IMAGE_VERSION is supplied by docker environment
 
 APP_VERSION=$(cat ${APP_ROOT}/VERSION)
 SCHEMA_VERSION=$(cd ${APP_ROOT} ; bin/rake db:version | awk '{ print $3 }')
@@ -50,19 +58,49 @@ if [[ -z $APP_VERSION || -z $SCHEMA_VERSION || -z $IMAGE_VERSION ]]; then
   echo "${DEPLOY_INFO_FILE} is incomplete, one or more required variables are undefined"
   exit 1
 else
-  echo "APP_VERSION=${APP_VERSION}" > ${DEPLOY_INFO_FILE}
-  echo "SCHEMA_VERSION=${SCHEMA_VERSION}" >> ${DEPLOY_INFO_FILE}
-  echo "IMG_VERSION=${IMAGE_VERSION}" >> ${DEPLOY_INFO_FILE}
+  echo "PV_APP_VERSION=${APP_VERSION}" > ${DEPLOY_INFO_FILE}
+  echo "PV_SCHEMA_VERSION=${SCHEMA_VERSION}" >> ${DEPLOY_INFO_FILE}
+  echo "PV_IMG_VERSION=${IMAGE_VERSION}" >> ${DEPLOY_INFO_FILE}
 fi
+
+}
+
+function prepare_svc_vars {
+
+# Description
+# Prepare service host variables for use in other functions
+# *_SERVICE_NAME variables are supplied via OpenShift template parameters (i.e DATABASE_SERVICE_NAME)
+# *_SERVICE_HOST variables are auto-created by OpenShift and contain the IP address exposed by service pod
+# Upcase values of SERVICE_NAME, assemble proper SVC_HOST and export vars for use
+
+export DATABASE_SVC_HOST="$(echo $DATABASE_SERVICE_NAME | tr '[:lower:]' '[:upper:]')_SERVICE_HOST"
+export MEMCACHED_SVC_HOST="$(echo $MEMCACHED_SERVICE_NAME | tr '[:lower:]' '[:upper:]')_SERVICE_HOST"
+
+}
+
+function setup_memcached {
+# Description
+# Replace memcached host in EVM configuration to use assigned service pod IP
+
+echo "== Applying memcached config =="
+sed -i~ -E "s/:memcache_server:.*/:memcache_server: ${!MEMCACHED_SVC_HOST}:11211/gi" ${APP_ROOT}/config/settings.yml
+
 }
 
 function setup_persistent_data {
+# Description
+# Process container.data.persist which contains the desired files/dirs to store on PV
+# Copy listed files/dirs to PV, make backups and deploy symblinks pointing to PV
 
-APP_ROOT_PERSISTENT="/var/www/miq/vmdb.test/persistent"
+# This file is supplied by the container image with default files/dirs
 CONTAINER_INPUT_FILE="/container.data.persist"
-PERSIST_INPUT_FILE="$APP_ROOT_PERSISTENT/container.data.persist"
+
+# Copy of CONTAINER_INPUT_FILE that will be stored on PV and can be customized by users to add their own files/dirs
+PERSISTENT_INPUT_FILE="$APP_ROOT_PERSISTENT/container.data.persist"
 
  [ ! -f "${PERSIST_INPUT_FILE}" ] && cp -a ${CONTAINER_INPUT_FILE} ${APP_ROOT_PERSISTENT}
+
+echo "== Initializing persistent data =="
 
 while read -r file
 do
@@ -76,12 +114,12 @@ do
     FILENAME=$(basename $file)
     # Create directory structure under persistent volume if not present
     [[ ! -d ${APP_ROOT_PERSISTENT}/${DIR} ]] && mkdir -p ${APP_ROOT_PERSISTENT}/${DIR}
-    # Copy data into persistent volume
+    # Copy supplied files/dirs into persistent volume
     cp -a $file ${APP_ROOT_PERSISTENT}${DIR}/$FILENAME
     # Check if we are working with a directory, backup
     [[ -d $file ]] && mv ${file} ${file}~
     # Place symblink back to persistent volume
     ln --backup -sn ${APP_ROOT_PERSISTENT}${DIR}/$FILENAME $file
-done < "${PERSIST_INPUT_FILE}"
+done < "${PERSISTENT_INPUT_FILE}"
 
 }
