12c5146a254a2f4339f0faf70e906578cfe32ca9
Nick Carboni <ncarboni@redhat.com>
2017-04-13 10:45:01 -0400

    Indent case blocks

--- images/miq-app/docker-assets/appliance-initialize.sh
+++ images/miq-app/docker-assets/appliance-initialize.sh
@@ -26,52 +26,52 @@ check_if_new_replica
 # Select path of action based on DEPLOYMENT_STATUS value
 case "${DEPLOYMENT_STATUS}" in
   redeployment)
-  echo "== Starting Re-deployment =="
-  restore_pv_data
-  setup_memcached
+    echo "== Starting Re-deployment =="
+    restore_pv_data
+    setup_memcached
   ;;
   upgrade)
-  echo "== Starting Upgrade =="
-  backup_pv_data
-  run_hook pre-upgrade
-  restore_pv_data
-  setup_memcached
-  migrate_db
-  run_hook post-upgrade
-  write_deployment_info
+    echo "== Starting Upgrade =="
+    backup_pv_data
+    run_hook pre-upgrade
+    restore_pv_data
+    setup_memcached
+    migrate_db
+    run_hook post-upgrade
+    write_deployment_info
   ;;
   new_replica)
-  echo "== Starting New Replica =="
-  setup_logs
-  setup_memcached
-  replica_join_region
-  sync_pv_data
-  restore_pv_data
+    echo "== Starting New Replica =="
+    setup_logs
+    setup_memcached
+    replica_join_region
+    sync_pv_data
+    restore_pv_data
   ;;
   new_deployment)
-  echo "== Starting New Deployment =="
-  # Setup logs on PV before init
-  setup_logs
+    echo "== Starting New Deployment =="
+    # Setup logs on PV before init
+    setup_logs
 
-  # Setup memcached host in EVM configuration
-  setup_memcached
+    # Setup memcached host in EVM configuration
+    setup_memcached
 
-  # Run appliance_console_cli to init appliance
-  init_appliance
+    # Run appliance_console_cli to init appliance
+    init_appliance
 
-  # Init persistent data from application rootdir on PV
-  init_pv_data
+    # Init persistent data from application rootdir on PV
+    init_pv_data
 
-  # Make initial backup
-  backup_pv_data
+    # Make initial backup
+    backup_pv_data
 
-  # Restore symlinks from PV to application rootdir
-  restore_pv_data
+    # Restore symlinks from PV to application rootdir
+    restore_pv_data
 
-  # Write deployment info file to PV
-  write_deployment_info
+    # Write deployment info file to PV
+    write_deployment_info
   ;;
   *)
-  echo "Could not find a suitable deployment type, exiting.."
-  exit 1
+    echo "Could not find a suitable deployment type, exiting.."
+    exit 1
 esac
