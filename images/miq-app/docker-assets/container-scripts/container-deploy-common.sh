406ce3bedb29263ace0c69d5b4165cb1ccff3ab3
Nick Carboni <ncarboni@redhat.com>
2017-07-11 17:28:11 -0400

    Revert to DeploymentConfig and remove server PV
    This makes the entire app act as one server. The GUID is generated
    using a template parameter and is provided to the server pod using
    an environment variable.
    
    We ignore logs because we will be printing our logs to STDOUT which
    will be processed by the EFK log aggregation stack.
    
    We also remove certs because they will be handled by the httpd
    pod or by the route itself.
    
    This allows us to remove the "server" PV entirely.

--- images/miq-app/docker-assets/container-scripts/container-deploy-common.sh
+++ images/miq-app/docker-assets/container-scripts/container-deploy-common.sh
@@ -2,23 +2,9 @@
 
 [[ -s /etc/default/evm ]] && source /etc/default/evm
 
-# This directory is used to store server specific data to be persisted
-PV_CONTAINER_DATA_DIR="${APP_ROOT_PERSISTENT}/server-data"
-
-# This directory is used to store server specific container deployment data (logs,backups,etc)
-PV_CONTAINER_DEPLOY_DIR="${APP_ROOT_PERSISTENT}/server-deploy"
-
-# This directory is used to store server specific initialization logfiles on PV
-PV_LOG_DIR="${PV_CONTAINER_DEPLOY_DIR}/log"
-
-# Directory used to backup server specific PV data before performing an upgrade
-PV_BACKUP_DIR="${PV_CONTAINER_DEPLOY_DIR}/backup"
-
-# This file is supplied by the app docker image with default files/dirs to persist on PV
-DATA_PERSIST_FILE="/container.data.persist"
-
-# Set log timestamp for running instance
-PV_LOG_TIMESTAMP="$(date +%s)"
+function write_guid() {
+  echo "${GUID}" > ${APP_ROOT}/GUID
+}
 
 function write_v2_key() {
   echo "== Writing encryption key =="
@@ -47,30 +33,6 @@ function check_svc_status() {
   echo "${SVC_NAME}:${SVC_PORT} - accepting connections"
 }
 
-# Join the new server/replica to the remote region
-function replica_join_region() {
-  echo "== Joining region =="
-  cd ${APP_ROOT} && RAILS_USE_MEMORY_STORE=true bin/rake evm:join_region
-}
-
-# Prepare appliance initialization environment
-function prepare_init_env() {
-  # Create container deployment dirs into PV if not already present
-  [ ! -d "${PV_LOG_DIR}" ] && mkdir -p "${PV_LOG_DIR}"
-  [ ! -d "${PV_BACKUP_DIR}" ] && mkdir -p "${PV_BACKUP_DIR}"
-}
-
-# Configure EVM logdir on PV
-function setup_logs() {
-  # Ensure EVM logdir is setup on PV before init
-  if [ ! -h "${APP_ROOT}/log" ]; then
-    [ ! -d "${PV_CONTAINER_DATA_DIR}${APP_ROOT}/log" ] && mkdir -p "${PV_CONTAINER_DATA_DIR}${APP_ROOT}/log"
-    cp -a "${APP_ROOT}/log" "${PV_CONTAINER_DATA_DIR}${APP_ROOT}"
-    mv "${APP_ROOT}/log" "${APP_ROOT}/log~"
-    ln --backup -sn "${PV_CONTAINER_DATA_DIR}${APP_ROOT}/log" "${APP_ROOT}/log"
-  fi
-}
-
 # Execute appliance_console to initialize appliance
 function init_appliance() {
   echo "== Initializing Appliance =="
@@ -94,30 +56,15 @@ function run_hook() {
   HOOK_SCRIPT="${CONTAINER_SCRIPTS_ROOT}/$SCRIPT_NAME"
   PV_HOOK_SCRIPT_LOG="${PV_LOG_DIR}/${SCRIPT_NAME}_hook_${PV_LOG_TIMESTAMP}.log"
 
-  (
-    if [ -f "${HOOK_SCRIPT}" ]; then
-      # Ensure is executable
-      [ ! -x "${HOOK_SCRIPT}" ] && chmod +x "${HOOK_SCRIPT}"
-      echo "== Running ${HOOK_SCRIPT} =="
-      ${HOOK_SCRIPT}
-      [ "$?" -ne "0" ] && echo "ERROR: ${HOOK_SCRIPT} failed, please check logs at ${PV_HOOK_SCRIPT_LOG}" && exit 1
-    else
-      echo "Hook script ${SCRIPT_NAME} not found, skipping"
-    fi
-  ) 2>&1 | tee "${PV_HOOK_SCRIPT_LOG}"
-}
-
-# Execute DB migration, log output and check errors
-function migrate_db() {
-  PV_MIGRATE_DB_LOG="${PV_LOG_DIR}/migrate_db_${PV_LOG_TIMESTAMP}.log"
-
-  (
-    echo "== Migrating Database =="
-
-    cd ${APP_ROOT} && bin/rake db:migrate
-
-    [ "$?" -ne "0" ] && echo "ERROR: Failed to migrate database, please check logs at ${PV_MIGRATE_DB_LOG}" && exit 1
-  ) 2>&1 | tee ${PV_MIGRATE_DB_LOG}
+  if [ -f "${HOOK_SCRIPT}" ]; then
+    # Ensure is executable
+    [ ! -x "${HOOK_SCRIPT}" ] && chmod +x "${HOOK_SCRIPT}"
+    echo "== Running ${HOOK_SCRIPT} =="
+    ${HOOK_SCRIPT}
+    [ "$?" -ne "0" ] && echo "ERROR: ${HOOK_SCRIPT} failed" && exit 1
+  else
+    echo "Hook script ${SCRIPT_NAME} not found, skipping"
+  fi
 }
 
 # Set EVM admin pwd
@@ -129,70 +76,11 @@ function set_admin_pwd() {
    [ "$?" -ne "0" ] && echo "ERROR: Failed to set admin password, please check appliance logs"
 }
 
-# Process DATA_PERSIST_FILE which contains the desired files/dirs to store on the PV
-# Use rsync to transfer files/dirs, log output and check return status
-# Ensure we always store an initial data backup on PV
-function init_pv_data() {
-  PV_DATA_INIT_LOG="${PV_LOG_DIR}/init_pv_data_${PV_LOG_TIMESTAMP}.log"
+# Execute DB migration, log output and check errors
+function migrate_db() {
+  echo "== Migrating Database =="
 
-  (
-    echo "== Initializing PV data =="
+  cd ${APP_ROOT} && bin/rake db:migrate
 
-    sync_pv_data
-
-  ) 2>&1 | tee "${PV_DATA_INIT_LOG}"
-}
-
-# Process DATA_PERSIST_FILE which contains the desired files/dirs to restore from PV
-# Check if file/dir exists on PV, redeploy symlinks on ${APP_ROOT} pointing to PV
-function restore_pv_data() {
-  PV_DATA_RESTORE_LOG="${PV_LOG_DIR}/restore_pv_data_${PV_LOG_TIMESTAMP}.log"
-
-  (
-    echo "== Restoring PV data symlinks =="
-    while read -r FILE
-    do
-      # Sanity checks
-      [[ ${FILE} = \#* ]] && continue
-      [[ ${FILE} == / ]] && continue
-      [[ -h ${FILE} ]] && echo "${FILE} symlink is already in place, skipping" && continue
-      [[ ! -e ${PV_CONTAINER_DATA_DIR}$FILE ]] && echo "${FILE} does not exist on PV, skipping" && continue
-      # Obtain dirname and filename from source file
-      DIR="$(dirname ${FILE})"
-      FILENAME="$(basename ${FILE})"
-      # Check if we are working with a directory, backup
-      [[ -d ${FILE} ]] && mv "${FILE}" "${FILE}~"
-      # Place symlink back to persistent volume
-      ln --backup -sn "${PV_CONTAINER_DATA_DIR}${DIR}/${FILENAME}" "${FILE}"
-    done < "${DATA_PERSIST_FILE}"
-  ) 2>&1 | tee "${PV_DATA_RESTORE_LOG}"
-}
-
-# Backup existing PV data before initiating an upgrade procedure
-# Exclude EVM server logs
-function backup_pv_data() {
-  PV_DATA_BACKUP_LOG="${PV_LOG_DIR}/backup_pv_data_${PV_LOG_TIMESTAMP}.log"
-  PV_BACKUP_TIMESTAMP="$(date +%Y_%m_%d_%H%M%S)"
-
-  (
-    echo "== Initializing PV data backup =="
-
-    rsync -av --exclude 'log' "${PV_CONTAINER_DATA_DIR}" "${PV_BACKUP_DIR}/backup_${PV_BACKUP_TIMESTAMP}"
-
-    [ "$?" -ne "0" ] && echo "WARNING: Some files might not have been copied please check logs at ${PV_DATA_BACKUP_LOG}"
-  ) 2>&1 | tee "${PV_DATA_BACKUP_LOG}"
-}
-
-# Process data persist file and sync data back to PV
-# Skip essential files that should never be synced back after region initialization
-function sync_pv_data() {
-  PV_DATA_SYNC_LOG="${PV_LOG_DIR}/sync_pv_data_${PV_LOG_TIMESTAMP}.log"
-
-  (
-    echo "== Syncing persist data to server PV =="
-
-    rsync -qavL --files-from="${DATA_PERSIST_FILE}" / "${PV_CONTAINER_DATA_DIR}"
-
-    [ "$?" -ne "0" ] && echo "WARNING: Some files might not have been copied please check logs at ${PV_DATA_SYNC_LOG}"
-  ) 2>&1 | tee "${PV_DATA_SYNC_LOG}"
+  [ "$?" -ne "0" ] && echo "ERROR: Failed to migrate database" && exit 1
 }
