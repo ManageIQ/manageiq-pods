2848d7a0a73d92ae3bd246194ac9c8cb92983be6
Nick Carboni <ncarboni@redhat.com>
2017-09-20 15:52:29 -0400

    Remove unnecessary service name environment variables
    Now that https://github.com/ManageIQ/manageiq/pull/16001 is merged
    we can remove the environment variables that were previously being
    used in place of the *_SERVICE_HOST ones

--- images/miq-app-frontend/docker-assets/check-dependent-services.sh
+++ images/miq-app-frontend/docker-assets/check-dependent-services.sh
@@ -4,5 +4,5 @@
 [[ -s ${CONTAINER_SCRIPTS_ROOT}/container-deploy-common.sh ]] && source "${CONTAINER_SCRIPTS_ROOT}/container-deploy-common.sh"
 
 # Check readiness of external services
-check_svc_status ${MEMCACHED_SERVICE_NAME} 11211
-check_svc_status ${DATABASE_SERVICE_NAME} 5432
+check_svc_status ${MEMCACHED_SERVICE_HOST} ${MEMCACHED_SERVICE_PORT}
+check_svc_status ${POSTGRESQL_SERVICE_HOST} ${POSTGRESQL_SERVICE_PORT}
