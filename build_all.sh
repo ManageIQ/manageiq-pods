2ede1ba22a79cf0f74696f14b0267c6a58baefa4
Nick Carboni <ncarboni@redhat.com>
2018-02-15 16:13:32 -0500

    Use manageiq docker hub org

--- build_all.sh
+++ build_all.sh
@@ -3,13 +3,13 @@
 pushd images
   find . -name build.log | xargs rm
 
-  docker build manageiq-base -t carbonin/manageiq-base:latest --no-cache > manageiq-base/build.log
+  docker build manageiq-base -t manageiq/manageiq-base:latest --no-cache > manageiq-base/build.log
 
-  docker build manageiq-orchestrator -t carbonin/manageiq-orchestrator:latest > manageiq-orchestrator/build.log && docker push carbonin/manageiq-orchestrator:latest >> manageiq-orchestrator/build.log &
+  docker build manageiq-orchestrator -t manageiq/manageiq-orchestrator:latest > manageiq-orchestrator/build.log && docker push manageiq/manageiq-orchestrator:latest >> manageiq-orchestrator/build.log &
 
-  docker build manageiq-base-worker -t carbonin/manageiq-base-worker:latest > manageiq-base-worker/build.log && docker push carbonin/manageiq-base-worker:latest >> manageiq-base-worker/build.log
+  docker build manageiq-base-worker -t manageiq/manageiq-base-worker:latest > manageiq-base-worker/build.log && docker push manageiq/manageiq-base-worker:latest >> manageiq-base-worker/build.log
 
-  docker build manageiq-webserver-worker -t carbonin/manageiq-webserver-worker:latest > manageiq-webserver-worker/build.log && docker push carbonin/manageiq-webserver-worker:latest >> manageiq-webserver-worker/build.log
+  docker build manageiq-webserver-worker -t manageiq/manageiq-webserver-worker:latest > manageiq-webserver-worker/build.log && docker push manageiq/manageiq-webserver-worker:latest >> manageiq-webserver-worker/build.log
 
-  docker build manageiq-ui-worker -t carbonin/manageiq-ui-worker:latest > manageiq-ui-worker/build.log && docker push carbonin/manageiq-ui-worker:latest >> manageiq-ui-worker/build.log
+  docker build manageiq-ui-worker -t manageiq/manageiq-ui-worker:latest > manageiq-ui-worker/build.log && docker push manageiq/manageiq-ui-worker:latest >> manageiq-ui-worker/build.log
 popd
