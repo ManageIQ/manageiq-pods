e1f596a5606f2890aa9a3752af7ceb1447ccf0e8
Nick Carboni <ncarboni@redhat.com>
2019-10-31 16:37:27 -0400

    Consolidate orchestrator startup scripts
    Many of the functions used by the orchestrator entrypoint script
    were only used by the orchestrator, but were defined in the
    base image.
    
    This commit moves the functions or the content of the functions
    directly into the orchestrator entrypoint script and does the
    same for scripts directly in the orchestrator image which were
    unnecessarily split out.
    
    This makes the orchestrator entrypoint more readable and maintainable

--- images/manageiq-orchestrator/container-assets/check-dependent-services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Source OpenShift scripting env
-[[ -s ${CONTAINER_SCRIPTS_ROOT}/container-deploy-common.sh ]] && source "${CONTAINER_SCRIPTS_ROOT}/container-deploy-common.sh"
-
-# Check readiness of external services
-check_svc_status ${MEMCACHED_SERVICE_HOST} ${MEMCACHED_SERVICE_PORT}
-check_svc_status ${POSTGRESQL_SERVICE_HOST} ${POSTGRESQL_SERVICE_PORT}
