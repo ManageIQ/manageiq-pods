e80d326746a51c8592bdd084224deecc6ab61754
Nick Carboni <ncarboni@redhat.com>
2020-04-15 09:37:17 -0400

    Merge pull request #432 from gyliu513/olm
    Update operator-sdk to 0.15.1
    
    (cherry picked from commit 9514189e217dc754a831086f28fa5a1dd1e184f8)

--- manageiq-operator/pkg/apis/manageiq/v1alpha1/doc.go
+++ manageiq-operator/pkg/apis/manageiq/v1alpha1/doc.go
@@ -1,4 +1,4 @@
 // Package v1alpha1 contains API Schema definitions for the manageiq v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=manageiq.example.com
+// +groupName=manageiq.org
 package v1alpha1
