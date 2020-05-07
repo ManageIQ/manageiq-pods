a7a160f1210ac2d113b376fcc203c165af2bbea9
Satoe Imaishi <simaishi@redhat.com>
2020-05-07 12:33:40 -0400

    Support for using local RPMs

---
+++ images/manageiq-base/container-assets/create_local_yum_repo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum -y install createrepo_c
+createrepo /tmp/rpms
+yum -y remove createrepo_c
+
+cat > /etc/yum.repos.d/local_rpm.repo << EOF
+[local-rpm]
+baseurl=file:///tmp/rpms/$basearch
+name=Local yum repo
+enabled=1
+gpgcheck=0
+EOF
+
+dnf config-manager --setopt=copr:copr.fedorainfracloud.org:simaishi:test.exclude=manageiq-* --save
