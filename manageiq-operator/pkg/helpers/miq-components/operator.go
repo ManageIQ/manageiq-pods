da9692828d44c4926ea9e62b0448b245a4f5783f
Brandon Dunne <brandondunne@hotmail.com>
2020-09-23 16:33:38 -0400

    Extract fetching operator deployment resources for reuse

--- manageiq-operator/pkg/helpers/miq-components/operator.go
+++ manageiq-operator/pkg/helpers/miq-components/operator.go
@@ -12,20 +12,7 @@ import (
 )
 
 func ManageOperator(cr *miqv1alpha1.ManageIQ, client client.Client) (*appsv1.Deployment, controllerutil.MutateFn) {
-	operatorPodName := os.Getenv("POD_NAME")
-	podKey := types.NamespacedName{Namespace: cr.Namespace, Name: operatorPodName}
-	pod := &corev1.Pod{}
-	client.Get(context.TODO(), podKey, pod)
-
-	operatorReplicaSetName := pod.ObjectMeta.OwnerReferences[0].Name
-	replicaSetKey := types.NamespacedName{Namespace: cr.Namespace, Name: operatorReplicaSetName}
-	replicaSet := &appsv1.ReplicaSet{}
-	client.Get(context.TODO(), replicaSetKey, replicaSet)
-
-	operatorDeploymentName := replicaSet.ObjectMeta.OwnerReferences[0].Name
-	deploymentKey := types.NamespacedName{Namespace: cr.Namespace, Name: operatorDeploymentName}
-	deployment := &appsv1.Deployment{}
-	client.Get(context.TODO(), deploymentKey, deployment)
+	deployment := operatorDeployment(cr, client)
 
 	f := func() error {
 		addBackupLabel(cr.Spec.BackupLabelName, &deployment.ObjectMeta)
@@ -76,4 +63,33 @@ func OidcCaCertSecret(cr *miqv1alpha1.ManageIQ, client client.Client) (*corev1.S
 	}
 
 	return secret, f
+}
+
+func operatorPod(cr *miqv1alpha1.ManageIQ, client client.Client) *corev1.Pod {
+	operatorPodName := os.Getenv("POD_NAME")
+	podKey := types.NamespacedName{Namespace: cr.Namespace, Name: operatorPodName}
+	pod := &corev1.Pod{}
+	client.Get(context.TODO(), podKey, pod)
+
+	return pod
+}
+
+func operatorReplicaSet(cr *miqv1alpha1.ManageIQ, client client.Client) *appsv1.ReplicaSet {
+	pod := operatorPod(cr, client)
+	operatorReplicaSetName := pod.ObjectMeta.OwnerReferences[0].Name
+	replicaSetKey := types.NamespacedName{Namespace: cr.Namespace, Name: operatorReplicaSetName}
+	replicaSet := &appsv1.ReplicaSet{}
+	client.Get(context.TODO(), replicaSetKey, replicaSet)
+
+	return replicaSet
+}
+
+func operatorDeployment(cr *miqv1alpha1.ManageIQ, client client.Client) *appsv1.Deployment {
+	replicaSet := operatorReplicaSet(cr, client)
+	operatorDeploymentName := replicaSet.ObjectMeta.OwnerReferences[0].Name
+	deploymentKey := types.NamespacedName{Namespace: cr.Namespace, Name: operatorDeploymentName}
+	deployment := &appsv1.Deployment{}
+	client.Get(context.TODO(), deploymentKey, deployment)
+
+	return deployment
 }
