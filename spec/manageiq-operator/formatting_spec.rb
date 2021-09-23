79cd11d6ad4be143973d08436991f6c784ec2a8d
Jason Frey <fryguy9@gmail.com>
2021-09-23 12:17:39 -0400

    Add spec to check go mod tidy

---
+++ spec/manageiq-operator/formatting_spec.rb
@@ -0,0 +1,18 @@
+require 'awesome_spawn'
+
+describe "Go formatting check" do
+  it "has no source changes" do
+    result = AwesomeSpawn.run!("gofmt -l manageiq-operator", :chdir => ROOT)
+    expect(result.error).to be_empty
+    expect(result.output).to be_empty
+  end
+
+  it "has no mod/sum changes" do
+    result = AwesomeSpawn.run!("go mod tidy", :chdir => ROOT.join("manageiq-operator"))
+    expect(result.output).to be_empty
+
+    expect(
+      AwesomeSpawn.run!("git diff manageiq-operator/go.mod manageiq-operator/go.sum").output
+    ).to be_empty, "Files differ after running go mod tidy"
+  end
+end
