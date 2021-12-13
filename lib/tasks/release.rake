ManageIQ/manageiq-pods

commit 40d83bdb5361f0caaa4b96893b240a7e8593a204

Changes for new branch morphy

bin/build
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-TAG=latest-lasker
+TAG=latest-morphy-lasker
 
-BUILD_REF=${BUILD_REF:-lasker}
+BUILD_REF=${BUILD_REF:-morphy]}
 BUILD_ORG=${BUILD_ORG:-ManageIQ}
 
 ARCH=`uname -m`

bin/remove_images
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-TAG=latest-lasker
+TAG=latest-morphy-lasker
 
 while getopts "t:r:h" opt; do
   case $opt in

images/manageiq-base-worker/Dockerfile
@@ -1,5 +1,5 @@
 ARG FROM_REPO=manageiq
-ARG FROM_TAG=latest-lasker
+ARG FROM_TAG=latest-morphy-lasker
 
 FROM ${FROM_REPO}/manageiq-base:${FROM_TAG} AS vddk
 

images/manageiq-base/Dockerfile
@@ -1,6 +1,6 @@
 FROM registry.access.redhat.com/ubi8/ubi:latest as appliance_build
 
-ARG BUILD_REF=lasker
+ARG BUILD_REF=morphy
 ARG BUILD_ORG=ManageIQ
 ARG CORE_REPO_NAME=manageiq
 ARG GIT_HOST=github.com
@@ -39,11 +39,11 @@ RUN dnf -y --disableplugin=subscription-manager install \
       http://mirror.centos.org/centos/8.3.2011/BaseOS/${ARCH}/os/Packages/centos-linux-repos-8-2.el8.noarch.rpm \
       http://mirror.centos.org/centos/8.3.2011/BaseOS/${ARCH}/os/Packages/centos-gpg-keys-8-2.el8.noarch.rpm \
       https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm \
-      https://rpm.manageiq.org/release/12-lasker/el8/noarch/manageiq-release-12.0-1.el8.noarch.rpm \
+      https://rpm.manageiq.org/release/13-morphy/el8/noarch/manageiq-release-13.0-1.el8.noarch.rpm \
       https://resources.ovirt.org/pub/yum-repo/ovirt-release44.rpm && \
     dnf -y --disableplugin=subscription-manager module enable nodejs:12 && \
     dnf -y --disableplugin=subscription-manager module enable ruby:2.6 && \
-    if [[ "$RELEASE_BUILD" != "true" ]]; then dnf config-manager --enable manageiq-12-lasker-nightly; fi && \
+    if [[ "$RELEASE_BUILD" != "true" ]]; then dnf config-manager --enable manageiq-13-morphy-nightly; fi && \
     dnf config-manager --setopt=ubi-8-*.exclude=dracut*,net-snmp*,perl-*,redhat-release* --save && \
     if [[ "$LOCAL_RPM" = "true" ]]; then /create_local_yum_repo.sh; fi && \
     dnf -y --disableplugin=subscription-manager --setopt=tsflags=nodocs install \

images/manageiq-orchestrator/Dockerfile
@@ -1,5 +1,5 @@
 ARG FROM_REPO=manageiq
-ARG FROM_TAG=latest-lasker
+ARG FROM_TAG=latest-morphy-lasker
 
 FROM ${FROM_REPO}/manageiq-base:${FROM_TAG}
 MAINTAINER ManageIQ https://manageiq.org

images/manageiq-ui-worker/Dockerfile
@@ -1,5 +1,5 @@
 ARG FROM_REPO=manageiq
-ARG FROM_TAG=latest-lasker
+ARG FROM_TAG=latest-morphy-lasker
 
 FROM ${FROM_REPO}/manageiq-webserver-worker:${FROM_TAG}
 MAINTAINER ManageIQ https://manageiq.org

images/manageiq-webserver-worker/Dockerfile
@@ -1,5 +1,5 @@
 ARG FROM_REPO=manageiq
-ARG FROM_TAG=latest-lasker
+ARG FROM_TAG=latest-morphy-lasker
 
 FROM ${FROM_REPO}/manageiq-base-worker:${FROM_TAG}
 MAINTAINER ManageIQ https://manageiq.org

manageiq-operator/deploy/olm-catalog/manageiq-operator/0.0.1/manageiq-operator.v0.0.1.clusterserviceversion.yaml
@@ -57,7 +57,7 @@ spec:
                       fieldPath: metadata.name
                 - name: OPERATOR_NAME
                   value: manageiq-operator
-                image: docker.io/manageiq/manageiq-operator:latest-lasker
+                image: docker.io/manageiq/manageiq-operator:latest-morphy-lasker
                 imagePullPolicy: Always
                 name: manageiq-operator
                 resources:

commit 833450b69269c7b57f5012ad923e4c7a59e1079e

Fix typo
Introduced in 40d83bdb5361f0caaa4b96893b240a7e8593a204

Presented as:
[1/2] STEP 6/7: ARG GIT_AUTH
--> Using cache 0a3b98757153fa94ceb77db0b738ea8a9332c96b95303bf16fd8bfa411357b5b
--> 0a3b9875715
[1/2] STEP 7/7: RUN mkdir build &&     if [[ -n "$GIT_AUTH" ]]; then GIT_HOST=${GIT_AUTH}@${GIT_HOST}; fi && curl -L https://${GIT_HOST}/${BUILD_ORG}/${CORE_REPO_NAME}-appliance-build/tarball/${BUILD_REF} | tar vxz -C build --strip 1
curl: (3) [globbing] unmatched close brace/bracket in column 68

gzip: stdin: unexpected end of file
tar: Child returned status 1
tar: Error is not recoverable: exiting now

bin/build
@@ -2,7 +2,7 @@
 
 TAG=latest-morphy
 
-BUILD_REF=${BUILD_REF:-morphy]}
+BUILD_REF=${BUILD_REF:-morphy}
 BUILD_ORG=${BUILD_ORG:-ManageIQ}
 
 ARCH=`uname -m`

lib/tasks/release.rake
@@ -64,7 +64,7 @@ namespace :release do
     build_script = root.join("bin", "build")
     content = build_script.read
     content.sub!(/^(TAG=).+$/, "\\1latest-#{branch}")
-    content.sub!(/(BUILD_REF:-)\w+(\})/, "\\1#{branch}]\\2")
+    content.sub!(/(BUILD_REF:-)\w+(\})/, "\\1#{branch}\\2")
     build_script.write(content)
 
     # Modify bin/remove_images