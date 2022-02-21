cca8c1ae87270bd12bde1a9b0ab5fd576d170d1d
Brandon Dunne <brandondunne@hotmail.com>
2022-02-21 14:36:34 -0500

    Ignore gofmt for generated files

--- spec/manageiq-operator/formatting_spec.rb
+++ spec/manageiq-operator/formatting_spec.rb
@@ -4,7 +4,7 @@ describe "Go formatting check" do
   it "has no source changes" do
     result = AwesomeSpawn.run!("gofmt -l manageiq-operator", :chdir => ROOT)
     expect(result.error).to be_empty
-    expect(result.output).to be_empty
+    expect(result.output.split("\n") - ["manageiq-operator/pkg/apis/manageiq/v1alpha1/zz_generated.deepcopy.go"]).to be_empty
   end
 
   it "has no mod/sum changes" do
