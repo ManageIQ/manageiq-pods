e3f2c07032aa365fb6c74d87b492a5216ae72a58
Brandon Dunne <brandondunne@hotmail.com>
2022-03-25 15:56:17 -0400

    Update tests for new framework

--- spec/manageiq-operator/operator_build_spec.rb
+++ spec/manageiq-operator/operator_build_spec.rb
@@ -1,6 +1,6 @@
 describe "Operator build check" do
   it "successfully builds" do
     require 'awesome_spawn'
-    expect { AwesomeSpawn.run!("go build -o build/_output/bin/manageiq-operator ./cmd/manager", :chdir => ROOT.join("manageiq-operator")) }.not_to raise_error
+    expect { AwesomeSpawn.run!("make build", :chdir => ROOT.join("manageiq-operator")) }.not_to raise_error
   end
 end
