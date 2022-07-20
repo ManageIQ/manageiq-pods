a6d37bc565502f67357c0e2e92199ca05d5e54d4
Brandon Dunne <brandondunne@hotmail.com>
2022-07-20 14:40:37 -0400

    Add a TODO for 'make manifests'

--- spec/manageiq-operator/crd_spec.rb
+++ spec/manageiq-operator/crd_spec.rb
@@ -1,6 +1,9 @@
 require 'awesome_spawn'
 describe "Generate CRDs" do
   it "doesn't change any tracked files" do
+    # TODO: Figure out how to make this generate the correct data.
+    #       We are running a namespaced operator, but don't want to specify the namespace name in files
+    #       since that's up to the person installing the app.  Also, don't unnecessarily explode the roles.
     # AwesomeSpawn.run!("make manifests", :chdir => ROOT.join("manageiq-operator"))
     AwesomeSpawn.run!("make generate", :chdir => ROOT.join("manageiq-operator"))
 
