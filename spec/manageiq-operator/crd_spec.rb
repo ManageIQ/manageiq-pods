215302ab5a6c7cb83212862c659f5c5c94de0241
Brandon Dunne <brandondunne@hotmail.com>
2022-07-29 11:57:22 -0400

    Fix core apigroup definition
    - Single quotes are not YAML dumped correctly, use double quotes instead
    - Prevent role explosion by listing them all on the same line
    
    The only remaining issues with `make manifests` are:
    - It adds creationTimestamp: null
    - It tries to add a namespace
    - It changes the role name

--- spec/manageiq-operator/crd_spec.rb
+++ spec/manageiq-operator/crd_spec.rb
@@ -3,8 +3,9 @@ describe "Generate CRDs" do
   it "doesn't change any tracked files" do
     # TODO: Figure out how to make this generate the correct data.
     #       We are running a namespaced operator, but don't want to specify the namespace name in files
-    #       since that's up to the person installing the app.  Also, don't unnecessarily explode the roles.
+    #       since that's up to the person installing the app.
     # AwesomeSpawn.run!("make manifests", :chdir => ROOT.join("manageiq-operator"))
+
     AwesomeSpawn.run!("make generate", :chdir => ROOT.join("manageiq-operator"))
 
     expect(AwesomeSpawn.run!("git diff").output).to be_empty, "Files differ after generating CRDs"
