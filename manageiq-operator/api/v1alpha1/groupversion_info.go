983442ebcbec6077dba158b4509526ace360f748
Jason Frey <fryguy9@gmail.com>
2023-02-16 16:31:00 -0500

    Merge pull request #914 from bdunne/go_1.19
    Upgrade Go
    
    (cherry picked from commit e90ed5437cb70cc441e0dbae76d9cce81943d3f2)

--- manageiq-operator/api/v1alpha1/groupversion_info.go
+++ manageiq-operator/api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the  v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=manageiq.org
+// +kubebuilder:object:generate=true
+// +groupName=manageiq.org
 package v1alpha1
 
 import (
