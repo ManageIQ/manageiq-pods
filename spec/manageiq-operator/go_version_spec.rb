b73c4c0f9bd7c8c9e9e8956084749ef2fdf9a90d
Brandon Dunne <brandondunne@hotmail.com>
2023-06-12 17:25:22 -0400

    Add a test to ensure that go versions are all the same

---
+++ spec/manageiq-operator/go_version_spec.rb
@@ -0,0 +1,11 @@
+describe "Go Version" do
+  it "matches go.mod" do
+    require 'awesome_spawn'
+    dockerfile_version = File.read(ROOT.join("manageiq-operator", "Dockerfile")).match(/^FROM.+golang:(\d+\.\d+).+/)[1]
+    mod_version        = File.read(ROOT.join("manageiq-operator", "go.mod")).match(/^go\s(\d+\.\d+)/)[1]
+    running_version    = AwesomeSpawn.run!("go version").output.match(/.*\sgo(\d+\.\d+).*/)[1]
+
+    expect(dockerfile_version).to eq(mod_version)
+    expect(running_version).to    eq(mod_version)
+  end
+end
