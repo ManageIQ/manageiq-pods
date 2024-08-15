0aec29e41460dd3e2aa76878a246098965a416d8
Brandon Dunne <brandondunne@hotmail.com>
2024-08-15 15:31:12 -0400

    Check go version in the operator directory

--- spec/manageiq-operator/go_version_spec.rb
+++ spec/manageiq-operator/go_version_spec.rb
@@ -3,7 +3,7 @@ describe "Go Version" do
     require 'awesome_spawn'
     dockerfile_version = File.read(ROOT.join("manageiq-operator", "Dockerfile")).match(/^FROM.+golang:(\d+\.\d+).+/)[1]
     mod_version        = File.read(ROOT.join("manageiq-operator", "go.mod")).match(/^go\s(\d+\.\d+)/)[1]
-    running_version    = AwesomeSpawn.run!("go version").output.match(/.*\sgo(\d+\.\d+).*/)[1]
+    running_version    = AwesomeSpawn.run!("go version", :chdir => ROOT.join("manageiq-operator")).output.match(/.*\sgo(\d+\.\d+).*/)[1]
 
     expect(dockerfile_version).to eq(mod_version)
     expect(running_version).to    eq(mod_version)
